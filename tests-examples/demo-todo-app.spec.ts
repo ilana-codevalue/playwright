3d53052f98cba15f025bd7244e71b8fcefe0303f
ilan.adasha <ilana@codevalue.com>
2024-08-13 18:43:50 +0300

    add first workflow, add test fix

--- tests-examples/demo-todo-app.spec.ts
+++ tests-examples/demo-todo-app.spec.ts
@@ -28,6 +28,10 @@ test.describe('New Todo', () => {
     await newTodo.fill(TODO_ITEMS[1]);
     await newTodo.press('Enter');
 
+     // Create 3rd todo.
+     await newTodo.fill(TODO_ITEMS[2]);
+     await newTodo.press('Enter');
+
     // Make sure the list now has two todo items.
     await expect(page.getByTestId('todo-title')).toHaveText([
       TODO_ITEMS[0],
